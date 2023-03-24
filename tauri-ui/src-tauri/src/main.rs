498fa4565326bc70e03e2b107d56d68ad1c0e010
sak96 <26397224+sak96@users.noreply.github.com>
2023-03-24 15:21:38 +0530

    Use Tauri events instead of polling.

--- tauri-ui/src-tauri/src/main.rs
+++ tauri-ui/src-tauri/src/main.rs
@@ -1,8 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod managed_proxy;
 mod proxy;
+
 fn main() {
     tauri::Builder::default()
         .plugin(proxy::init())
