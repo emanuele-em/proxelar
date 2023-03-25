540f897dc2f10ee2debcb88fa193371b4b6d5103
sak96 <26397224+sak96@users.noreply.github.com>
2023-03-25 22:34:35 +0530

    Add command to get proxy status.

--- tauri-ui/src-tauri/src/proxy.rs
+++ tauri-ui/src-tauri/src/proxy.rs
@@ -32,10 +32,6 @@ async fn start_proxy<R: Runtime>(
     });
 
     let mut proxy = proxy.lock().await;
-    #[cfg(not(debug_assertions))]
-    {
-        assert!(proxy.is_none());
-    }
     proxy.replace((close_tx, thread));
 
     tauri::async_runtime::spawn(async move {
@@ -57,12 +53,21 @@ async fn stop_proxy(proxy: State<'_, ProxyState>) -> Result<(), String> {
     Ok(())
 }
 
+#[tauri::command]
+async fn proxy_status(proxy: State<'_, ProxyState>) -> Result<bool, String> {
+    Ok(proxy.lock().await.is_some())
+}
+
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("proxy")
         .setup(|app_handle| {
             app_handle.manage(Mutex::new(None) as ProxyState);
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![start_proxy, stop_proxy,])
+        .invoke_handler(tauri::generate_handler![
+            start_proxy,
+            stop_proxy,
+            proxy_status
+        ])
         .build()
 }
